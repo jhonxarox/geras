13e20663e18dec40fcfb56da9c961439d1c6bcbc
jhonxarox <jhonarox22@gmail.com>
2025-01-27 21:39:28 +0800

    feat(filter): add filter for pokemon

--- src/app/features/pokemon-list/pokemon-list.component.ts
+++ src/app/features/pokemon-list/pokemon-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../../core/services/pokemon.service';
 import { CommonModule } from '@angular/common';
 import { forkJoin } from 'rxjs';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { FavoritesService } from '../../core/services/favorites.service';
 import { PokemonCardComponent } from '../../shared/pokemon-card/pokemon-card.component';
 
@@ -19,15 +19,23 @@ import { PokemonCardComponent } from '../../shared/pokemon-card/pokemon-card.com
 })
 export class PokemonListComponent implements OnInit {
   pokemonList: any[] = [];
+  filteredPokemonList: any[] = [];
   isLoading = false;
   offset = 0;
+  selectedType: string = 'all'; // Default to show all Pokémon
 
   constructor(
     private pokemonService: PokemonService,
-    private favoritesService: FavoritesService
+    private favoritesService: FavoritesService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.selectedType = params['type'] || 'all';
+      this.applyFilter();
+    });
+    
     this.loadPokemons();
     window.addEventListener('scroll', this.onScroll.bind(this));
   }
@@ -62,9 +70,21 @@ export class PokemonListComponent implements OnInit {
 
       forkJoin(detailRequests).subscribe((detailedPokemonList) => {
         this.pokemonList = [...this.pokemonList, ...detailedPokemonList];
+        this.filteredPokemonList = this.pokemonList;
         this.offset += 30;
         this.isLoading = false;
+        this.applyFilter();
       });
     });
+  }
+
+  applyFilter(): void {
+    if (this.selectedType === 'all') {
+      this.filteredPokemonList = this.pokemonList;
+    } else {
+      this.filteredPokemonList = this.pokemonList.filter((pokemon) =>
+        pokemon.types.some((type: any) => type.type.name === this.selectedType)
+      );
+    }
   }
 }
